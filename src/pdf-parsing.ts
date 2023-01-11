2f99b17c1ec8ed1960ce6c2dea0792af48f505f3
Hendri Pretorius <pretorh@gmail.com>
2023-01-11 15:58:11 +0200

    pdf: read password protected file
    add optional IReadOptions with optional password, and use that if a
    password is needed

--- src/pdf-parsing.ts
+++ src/pdf-parsing.ts
@@ -1,8 +1,17 @@
 import { Page, readPdfText } from 'pdf-text-reader';
 
+interface IReadOptions {
+  password?: string;
+}
+
 export default class PdfParser {
-  public static async readAllLines(file: string): Promise<string[]> {
-    const pages = await readPdfText(file);
+  public static async readAllLines(file: string, options: IReadOptions = {}): Promise<string[]> {
+    const passwordHandler = (callback: (password: string) => void) => {
+      callback(options.password || '');
+      return ''; // readPdfText requires a string result
+    };
+
+    const pages = await readPdfText(file, false, passwordHandler);
     return pages
       .reduce((lines: string[], page: Page) => lines.concat(page.lines), []);
   }
--- test/pdf-parsing.js
+++ test/pdf-parsing.js
@@ -4,6 +4,7 @@ const { PdfParser } = require('../dist');
 
 const testPdfFile = {
   file: './test/data/test-PDF-file.pdf',
+  protectedFile: './test/data/test-password-PDF-file.pdf',
   lines: {
     all: [
       'Test PDF file',
@@ -24,6 +25,13 @@ describe('PDF parsing', () => {
   describe('readAllLines', () => {
     it('returns all the lines in a PDF file', async () => {
       const lines = await PdfParser.readAllLines(testPdfFile.file);
+      expect(lines).to.have.same.members(testPdfFile.lines.all);
+    });
+  });
+
+  describe('password protected PDFs', () => {
+    it('can read lines when password given', async () => {
+      const lines = await PdfParser.readAllLines(testPdfFile.protectedFile, { password: 'OpenPassword' });
       expect(lines).to.have.same.members(testPdfFile.lines.all);
     });
   });
